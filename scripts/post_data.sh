19e1fba4ee150d07e9f512dc38ec484022438bad
junehan <junehan.dev@gmail.com>
2022-04-08 15:59:01 +0900

    NEW/test script/create/post

---
+++ scripts/post_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "test index.."
+curl 127.0.0.1:3000/
+echo "test index FIN"
+echo "test post create.."
+curl 127.0.0.1:3000/api/post --data title="test" --data content="test content\nthisismy content" --data author="june_test"
+echo "test post FIN"
--- src/post/api_router.js
+++ src/post/api_router.js
@@ -2,5 +2,5 @@ const express = require("express");
 const apiRouter = express.Router({"caseSensitive" : true, "strict" : true});
 const create = require("./services/create");
 
-apiRouter.route("/:category").get(create).post(create);
+apiRouter.route("/").get(create).post(create);
 module.exports = apiRouter;
