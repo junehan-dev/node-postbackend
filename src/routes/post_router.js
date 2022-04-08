dfe163fd29e89962dc056b30c7e83dcc7c1ee7e9
junehan <junehan.dev@gmail.com>
2022-04-09 00:00:16 +0900

    detail/findbyid

--- src/routes/post_router.js
+++ src/routes/post_router.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const Router = express.Router({"caseSensitive" : true, "strict" : true});
-const {listPost, createPost} = require("./services/post_service");
+const {listPost, createPost, detailPost} = require("./services/post_service");
 
 Router.route("/").get(listPost).post(createPost);
+Router.route("/:postId/").get(detailPost);
 
 module.exports = Router;
 
--- src/routes/services/post_service.js
+++ src/routes/services/post_service.js
@@ -28,5 +28,21 @@ function listPost(req, res) {
 	});
 }
 
+function detailPost(req, res) {
+	const model = ListModel;
+	const query = model.findById(req.params["postId"]).select(["author", "title", "created", "content"]);
+	query.exec((err, data) => {
+		if (err) {
+			console.log(err);
+			res.status(404).end();
+		} else {
+			console.log(data);
+			res.json(data);
+		}
+	});
+}
+
 module.exports.listPost = listPost;
 module.exports.createPost = createPost;
+module.exports.detailPost = detailPost;
+
