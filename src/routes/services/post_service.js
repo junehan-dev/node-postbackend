f732b4bd1f787037efa1bdc6b7b0bd59bfd2f18d
junehan <junehan.dev@gmail.com>
2022-04-09 14:26:29 +0900

    REMOVE/DEBUG lines

--- src/routes/services/post_service.js
+++ src/routes/services/post_service.js
@@ -1,30 +1,20 @@
 const { PostModel } = require("./models/post_model");
 
-console.log("SEE This message at first time.");
-
 function createPost(req, res) {
 	const model = new PostModel(req.body);
 	const prom = model.save();
 	prom.then((data) => {
-		console.log(data);
 		res.json(data);
-	}).catch((err) => {
-		console.log(err);
+	}).catch(() => {
 		res.status(404).end();
 	});
 }
 
 function listPost(req, res) {
 	const model = PostModel;
-	const query = model.find().select(["author", "title", "created"]).sort({created: "desc"}).limit(10);
+	const query = model.find().select(["author", "title", "created"]).sort({created: "desc"});
 	query.exec((err, data) => {
-		if (err) {
-			console.log(err);
-			res.status(404).end();
-		} else {
-			console.log(data);
-			res.json(data);
-		}
+		(err) ?  res.status(404).end() : res.json(data);
 	});
 }
 
@@ -32,43 +22,26 @@ function detailPost(req, res) {
 	const model = PostModel;
 	const query = model.findById(req.params["postId"]).select(["author", "title", "created", "content"]);
 	query.exec((err, data) => {
-		if (err) {
-			console.log(err);
-			res.status(404).end();
-		} else {
-			console.log(data);
-			res.json(data);
-		}
+		(err) ?	res.status(404).end() : res.json(data);
 	});
 }
 
 function updatePost(req, res) {
-	console.log(req.body);
 	const model = PostModel;
 	const query = model.findByIdAndUpdate(req.params["postId"], req.body);
 	query.exec((err, data) => {
-		if (err) {
-			console.log(err);
-			res.status(404).end();
-		} else {
-			console.log(data);
-			res.json(data);
-		}
+		(err) ? res.status(404).end() : res.json(data);
 	});
 }
 
 function deletePost(req, res) {
-	console.log(req);
 	const model = PostModel;
 	const query = model.findByIdAndRemove(req.params["postId"]);
 	query.exec((err, data) => {
-		if (err) {
-			console.log(err);
+		if (err)
 			res.status(404).end();
-		} else {
-			console.log(data);
-			(data === null) ? res.status(204).end() : res.status(200).end();
-		}
+		else
+			((data === null) ? res.status(204).end() : res.status(200).end());
 	});
 }
 
